a360e469c050d54d4f586df0ea61f7d1ad658871
aditya kumar <adityakr2019@gmail.com>
2024-07-04 19:32:14 +0530

    frist

---
+++ registration/src/App.js
@@ -0,0 +1,10 @@
+import Registration from "./pages/registration";
+function App() {
+  return ( 
+    <div>
+      <Registration/>
+    </div> 
+  );
+}
+
+export default App;
---
+++ registration/src/pages/registration.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import '../assets/styles/index.css'
+// import { NavLink } from 'react-router-dom';
+import { FaUser,FaLock } from "react-icons/fa";
+import { IoMdCall } from "react-icons/io";
+
+function Registration() {
+  return (
+    <>
+    <div className="login">
+        <img className="login__bg" src="https://raw.githubusercontent.com/bedimcode/animated-login-form/1b0a04c9d3f47258bd1da07550571c23fadd1327/assets/img/login-bg.png" alt="" />
+        <div  className="login__form1">
+           <h1 className="login__title1">Registration</h1>
+
+           <div className="login__inputs">
+              <div className="login__box">
+                <input type="text" placeholder="Name" required className="login__input"/>
+                <FaUser className='icon' />
+              </div>
+              <div className="login__box">
+                <input type="tex" placeholder="Mobile" required className="login__input"/>
+                <IoMdCall className='icon' />
+              </div>
+
+              <div className="login__box">
+                <input type="email" placeholder="Email ID"   required className="login__input"/>
+                <i className="ri-mail-fill"></i>
+              </div>
+
+              <div className="login__box">
+                 <input type="password" placeholder="Password"  required className="login__input"/>
+                 <FaLock className='icon' />
+              </div>
+           </div>
+
+           <div className="login__check">
+              <div className="login__check-box">
+                 <input type="checkbox" className="login__check-input" id="user-check"/>
+                 <label for="user-check" className="login__check-label">Remember me</label>
+              </div>
+           </div>
+           <button type="submit"  className="login__button">Register</button>
+           <div className="login__register">
+              Don't have an login?
+            </div>
+        </div>
+        
+     </div> 
+    
+    </>
+  )
+}
+
+export default Registration;
